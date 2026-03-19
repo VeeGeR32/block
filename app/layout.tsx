a347c147e42b51bf1968c58db983d0e79901221a
VeeGeR32 <dialloalioune32@icloud.com>
2026-03-19 03:43:53 +0100

    Fix TypeScript typage layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,7 @@
 import './globals.css';
 import { Providers } from './Providers';
 
-export default function RootLayout({ children }) {
-  return (
+export default function RootLayout({ children }: { children: React.ReactNode }) {  return (
     <html lang="fr">
       <body>
         <Providers>{children}</Providers>
